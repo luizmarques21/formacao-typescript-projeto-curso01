963583cd9abce43d792e6df7836da41d47e07ea1
Luiz Marques <mariel.marques21@gmail.com>
2025-07-14 21:55:11 -0300

    Validando interações

--- src/types/Conta.ts
+++ src/types/Conta.ts
@@ -2,6 +2,7 @@ import {Transacao} from "./Transacao.js";
 import {GrupoTransacao} from "./GrupoTransacao.js";
 import {TipoTransacao} from "./TipoTransacao.js";
 import {Armazenador} from "./Armazenador.js";
+import {ValidaDebito, ValidaDeposito} from "./Decorators.js";
 
 export class Conta {
     protected nome: string;
@@ -67,24 +68,15 @@ export class Conta {
         Armazenador.salvar('transacoes', JSON.stringify(this.transacoes));
     }
 
+    @ValidaDebito
     debitar(valor: number): void {
-        if (valor <= 0) {
-            throw new Error('O valor a ser debitado deve ser maior que zero!');
-        }
-        if (valor > this.saldo) {
-            throw new Error('Saldo insuficiente!');
-        }
-
         this.saldo -= valor;
         Armazenador.salvar('saldo', this.saldo.toString());
     }
 
+    @ValidaDeposito
     depositar(valor: number): void {
-        if (valor <= 0) {
-            throw new Error('O valor a ser debitado deve ser maior que zero!');
-        }
-
-        this.saldo += valor;
+       this.saldo += valor;
         Armazenador.salvar('saldo', this.saldo.toString());
     }
 }
